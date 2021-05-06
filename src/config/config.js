1c17f4f9baa2e66169c4392a296941ce4c5eeca3
nellyganza <nishimwelys@gmail.com>
2021-05-06 17:22:42 +0200

    changes on sequelize config file

--- src/config/config.js
+++ src/config/config.js
@@ -37,6 +37,11 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     ssl:true, 
-    dialectOptions: {ssl: true}
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
   },
 };
